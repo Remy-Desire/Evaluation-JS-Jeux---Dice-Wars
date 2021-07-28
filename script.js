0ad68a262a0ede16a72a4dc888c03e1ebab731e7
Remy-Desire <airdesire38@gmail.com>
2021-07-28 14:38:59 +0200

    Fonction qui ajoute temporairement le score + Qui met a jour le compteur

--- script.js
+++ script.js
@@ -5,20 +5,26 @@ const dicePlayerOne = document.getElementById('dice-player-one');
 const launchPlayerTwo = document.getElementById('launch-player-two');
 const endOfPlayerTwo = document.getElementById('end-of-player-two');
 const dicePlayerTwo = document.getElementById('dice-player-two');
+const scorePlayerOne = document.getElementById('score-player-one');
+const scorePlayerTwo = document.getElementById('score-player-two');
 let valueDice;
-
+let score1 = 0;
+let score2 = 0;
+let temp = 0;
 
 //Evenement boutton Lancer Player 1
 launchPlayerOne.addEventListener('click', (e) => {
     valueDice = randomNumber(1, 6);
     changeDice(dicePlayerOne);
-    disabledLaunch(valueDice, launchPlayerOne);
+    disabledLaunch(valueDice, launchPlayerOne, launchPlayerTwo);
+    addTempScore(valueDice);
 })
 //Evenement boutton Lancer Player 2
 launchPlayerTwo.addEventListener('click', (e) => {
     valueDice = randomNumber(1, 6)
     changeDice(dicePlayerTwo)
-    disabledLaunch(valueDice, launchPlayerTwo);
+    disabledLaunch(valueDice, launchPlayerTwo, launchPlayerOne);
+    addTempScore(valueDice);
 })
 
 //Fonction générer un nombre aléatoir entre 1 et 6
@@ -51,10 +57,40 @@ function changeDice (element) {
 }
 
 // Fonction qui désactive le dé si = 1 
-function disabledLaunch (value, element) {
+function disabledLaunch (value, element, element2) {
     if(value === 1) {
-        return element.disabled = true;
+        element.disabled = true;
+        element2.disabled = false;
+        temp = 0;
     } else {
-        return element.disabled = false;
+        element.disabled = false;
+        element2.disabled = true;
     }
 }
+
+//Fonction qui ajoute temporairement le score du joueur
+function addTempScore (value){
+    if(value !== 1) {
+        temp += value;
+    } else {
+        temp = 0;
+    }
+    console.log(temp);
+} 
+
+//Fonction fin du tour player 1 
+endOfPlayerOne.addEventListener('click', (e) => {
+    score1 += temp;
+    launchPlayerOne.disabled = true;
+    launchPlayerTwo.disabled = false;
+    scorePlayerOne.innerHTML = score1;
+    temp = 0;
+})
+//Fonction fin du tour player 2
+endOfPlayerTwo.addEventListener('click', (e) => {
+    score1 += temp;
+    launchPlayerTwo.disabled = true;
+    launchPlayerOne.disabled = false;
+    scorePlayerTwo.innerHTML = score1;
+    temp = 0;
+})
